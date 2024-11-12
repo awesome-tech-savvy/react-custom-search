ee99b3115bbbb71a9371c06cbd7aece5869ffca7
awesome-tech-savvy <onetechsavvy@outlook.com>
2024-11-11 18:20:40 -0800

    complete custom search

--- src/components/SearchCustomer.js
+++ src/components/SearchCustomer.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CustomerList from './CustomerList'
 import List from '../List';
 
 function SearchCustomer() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredCustomers = List.filter(({name, location}) => {
+    return (
+      name.startsWith(searchTerm) ||
+      location.startsWith(searchTerm)
+    )
+  })
     
   return (
     <>
-    <div className='layout-row align-items-center justify-content-center mt-30'>
-        <input className='large mx-20 w-20' data-testid='search-input' value='Search' placeholder='Enter search term (Eg: Phil)' />
-    </div>
-    <CustomerList />
+      <div className='layout-row align-items-center justify-content-center mt-30'>
+          <input className='large mx-20 w-20' data-testid='search-input' value={searchTerm} placeholder='Enter search term (Eg: Phil)'
+            onChange={handleSearch}
+            type='text'/>
+      </div>
+      <CustomerList customers={filteredCustomers}/>
     </>
   )
 }
